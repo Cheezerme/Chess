bae1919fff03b6298b060aadf9b70d0bdb2fc6c0
Jeffrey Margotta <jeffreymargotta@gmail.com>
2019-08-03 19:12:28 -0400

    added skeleton of all pieces listed in chess, going to move on to initialize the board proper

--- Piece.rb
+++ Piece.rb
@@ -1,3 +1,4 @@
+require 'Rainbow'
 class Piece
 
   def initialize(color,board,pos)
@@ -7,13 +8,33 @@ class Piece
   end
 
   def to_s
-    
-  ##
+    #elegant way to display piece w/ proper coloring  
+  end
+  def valid_moves
+    #returns an array of all valid move positions for specified piece (self)
+  end
+  def pos=(val)
+    #unclear to me what this does atm
+  end
+
+  def symbol
+    #something about giving the piece a good visual for printing maybe?
+  end
+
+  private 
+  def move_into_check?(end_pos)
+    #method used to check if moving that piece into specified position will put
   end
 end
+
 class NullPiece < Piece
   def initialize
-    @type = nil
-    @team = nil
+    @value = nil
+  end
+  def symbol
+    #returns blank square for a null piece position
   end
 end
+
+class Rook_ < Piece
+  include
